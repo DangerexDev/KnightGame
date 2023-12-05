4f856e7c2a2b7563f5129a1f4b00369a093e1b1b
Rex Rockwell <rexjrockwell@outlook.com>
2023-12-04 23:53:35 -0800

    Update to Unreal 5.3

--- Source/KnightGame.Target.cs
+++ Source/KnightGame.Target.cs
@@ -8,8 +8,8 @@ public class KnightGameTarget : TargetRules
 	public KnightGameTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_2;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("KnightGame");
 	}
 }
